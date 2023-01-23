9c6b9f2aa3dcbbb4a87af61faadbd86c3f817a20
MohammadJavad <mrkhoshneshin1@gmail.com>
2023-01-22 23:36:04 -0800

    MohammadJavad: Add dependencies

---
+++ app/src/main/java/english/dictionary/app/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package english.dictionary.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val PurpleLight = Color(0xFFF1EDFA)
+val backgroundColor = Color(0xFFF0F0F0)
+val PurpleDark = Color(0xFF6A54A6)
+val PurpleMedium = Color(0x946A54A6)
+val Orange = Color(0xFFFF9800)
---
+++ app/src/main/java/english/dictionary/app/ui/theme/Type.kt
@@ -0,0 +1,34 @@
+package english.dictionary.app.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import english.dictionary.app.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
+val shabnam = FontFamily(
+    Font(R.font.shabnam_medium, FontWeight.Normal),
+    Font(R.font.shabnam_bold, FontWeight.Bold)
+)
