6bc4fb2b5fe321d3b510fe8bf504900cb6964b3e
MohammadJavad <mrkhoshneshin1@gmail.com>
2023-01-22 23:35:49 -0800

    MohammadJavad: Add dependencies

--- app/src/main/java/english/dictionary/app/MainActivity.kt
+++ app/src/main/java/english/dictionary/app/MainActivity.kt
@@ -2,10 +2,16 @@ package english.dictionary.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import english.dictionary.app.ui.theme.EnglishDictionaryTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            EnglishDictionaryTheme {
+
+            }
+        }
     }
 }
